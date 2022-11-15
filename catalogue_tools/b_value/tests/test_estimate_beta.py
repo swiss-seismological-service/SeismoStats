f35b8f1410b76f86daefb13ec8f0490d04ee2263
Aron Mirwald <aron.mirwald@sed.ethz.ch>
2022-11-15 18:23:36 +0100

    style: simplify beta and reapeat less in testfunctions

--- catalogue_tools/b_value/tests/test_estimate_beta.py
+++ catalogue_tools/b_value/tests/test_estimate_beta.py
@@ -8,49 +8,37 @@ from catalogue_tools.b_value.estimate_beta import estimate_beta_tinti
 from catalogue_tools.b_value.estimate_beta import estimate_beta_utsu
 from catalogue_tools.b_value.estimate_beta import estimate_beta_elst
 
+# parameters for testing
+n = 1000000
+beta = np.log(10)
+mc = 2.0
+precision = 0.005
+
 
 def test_estimate_beta_tinti():
-    n = 1000000
-    beta = 1 / np.log10(np.e)
-    mc = 2.0
-    precision = 0.005
-
     # generate synthetic magnitudes
     mags = simulate_magnitudes(n, beta, mc)
-
     # without binning
     delta_m = 0.0
     beta_estimate = estimate_beta_tinti(mags, mc, delta_m)
-    assert np.log10(np.e) * abs(beta - beta_estimate) <= precision
+    assert abs(beta - beta_estimate) / beta <= precision
 
 
 def test_estimate_beta_utsu():
-    n = 1000000
-    beta = 1 / np.log10(np.e)
-    mc = 2.0
-    precision = 0.005
-
     # generate synthetic magnitudes
     mags = simulate_magnitudes(n, beta, mc)
-
     # without binning
     delta_m = 0.0
     beta_estimate = estimate_beta_utsu(mags, mc, delta_m)
-    assert np.log10(np.e) * abs(beta - beta_estimate) <= precision
+    assert abs(beta - beta_estimate) / beta <= precision
 
 
 def test_estimate_beta_elst():
-    n = 1000000
-    beta = 1 / np.log10(np.e)
-    mc = 2.0
-    precision = 0.005
-
     # generate synthetic magnitudes
     mags = simulate_magnitudes(n, beta, mc)
-
     # without binning
     beta_estimate = estimate_beta_elst(mags)
-    assert np.log10(np.e) * abs(beta - beta_estimate) <= precision
+    assert abs(beta - beta_estimate) / beta <= precision
 
 
 if __name__ == '__main__':
