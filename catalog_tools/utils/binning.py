5c7846422a310225a3a062b1254a8bb139595bb6
Marta Han <marhan@eu-login-08.euler.ethz.ch>
2022-11-25 13:24:42 +0100

    style: fix spacing and a typo

--- catalog_tools/utils/binning.py
+++ catalog_tools/utils/binning.py
@@ -4,35 +4,35 @@ import numpy as np
 def normal_round_to_int(x: float) -> int:
     """
     Rounds a float number x to the closest integer.
-    
+
     Args:
         x: decimal number that needs to be rounded
-        
+    
     Returns:
-        Rounded value of the given number. 
+        Rounded value of the given number.
     """
-    
+
     sign = 1 if (x >= 0) else -1
-    y = abs(x)
-    
-    if x - math.floor(x) < 0.5:
-        return sign * np.floor(x)
-    return sign * np.ceil(x)
+    x = abs(x)
+
+    if x - np.floor(x) < 0.5:
+        return int(sign * np.floor(x))
+    return int(sign * np.ceil(x))
 
 
 def normal_round(x: float, n: int) -> float:
     """
     Rounds a float number x to n number of decimals. If the number
     of decimals is not given, we round to an integer.
-    
+
     Args:
         x: decimal number that needs to be rounded
         n: number of decimals, optional
-        
-    Returns:
-        Value rounded to the given number of decimals. 
-    """
     
+    Returns:
+        Value rounded to the given number of decimals.
+    """
+
     power = 10**n
     return normal_round_to_int(x * power) / power
     
@@ -41,15 +41,15 @@ def bin_to_precision(x: float, delta_m: float = 0.1) -> float:
     """
     Rounds a float number x to a given precision. If precision not given,
     assumes 0.1 bin size
-    
+
     Args:
         x: decimal number that needs to be rounded
         delta_m: size of the bin, optional
-        
-    Returns:
-        Value rounded to the given precision. 
-    """
     
+    Returns:
+        Value rounded to the given precision.
+    """
+
     return normal_round_to_int(x / delta_m) * delta_m
 
 
@@ -57,14 +57,14 @@ def bin_magnitudes(mags: list, delta_m: float = 0.1) -> list:
     """
     Rounds a list of float numbers to a given precision. 
     If precision not given, assumes 0.1 bin size.
-    
+
     Args:
         mags: list of values that need rounding
         delta_m: size of the bin, optional
-        
+   
     Returns:
-        List of values rounded to the given precision. 
+        List of values rounded to the given precision.
     """
-    
+
     rounded_list = [bin_to_precision(m, delta_m) for m in mags]
     return rounded_list
