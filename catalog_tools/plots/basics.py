74f0ffc5e45ecef8185d898a191cdda1f9262b58
adzubay <adzubay@gmail.com>
2023-06-13 11:18:57 +0200

    added get_cum_fmd and plot_cum_fmd_classic

--- catalog_tools/plots/basics.py
+++ catalog_tools/plots/basics.py
@@ -87,6 +87,108 @@ def plot_cum_fmd(
     return ax
 
 
+def get_cum_fmd(
+        mags: np.ndarray,
+        delta_m: float,
+        left: bool = False
+) -> [np.ndarray, np.ndarray]:
+    """ Calculates cumulative event counts across all magnitude units (summed from the right).
+    Note that the returned bins array contains the center point of each bin unless left is True.
+    Moreover, note that all bins except for the last are half-open in np.histogram
+    (right edge not included).
+
+    Args:
+        mags    : array of magnitudes
+        delta_m : discretization of the magnitudes
+        left    : When True, left edges of bins are returned. When false,
+                center points are returned.
+
+    Returns:
+        bins    : array of bin centers
+        c_counts: cumulative counts for each bin
+    """
+    bins = bin_to_precision(np.arange((np.min(mags)) * 1000,
+                                      (np.max(mags)+2*delta_m) * 1000,
+                                      delta_m * 1000) / 1000, delta_m)
+    counts = np.histogram(mags, bins)[0][::-1]
+    bins = bins[:-1][::-1]
+    c_counts = np.cumsum(counts)
+
+    if left:
+        pass
+    else:
+        bins = bins + delta_m / 2
+
+    return bins, c_counts
+
+
+def plot_cum_fmd_classic(
+        mags: np.ndarray,
+        ax: Optional[plt.Axes] = None,
+        b_value: Optional[float] = None,
+        mc: Optional[float] = None,
+        delta_m: float = 0.1,
+        color: Union[str, list] = 'blue',
+        size: int = 3,
+        grid: bool = False,
+        left: bool = False
+) -> plt.Axes:
+    """ Plots cumulative frequency magnitude distribution, optionally with a
+    corresponding theoretical Gutenberg-Richter (GR) distribution (using the
+    provided b-value). Unlike plot_cum_fmd, plots values for all bins and
+    requires binning.
+
+    Args:
+        mags    : array of magnitudes
+        ax      : axis where figure should be plotted
+        b_value : b-value of the theoretical GR distribution to plot
+        mc      : completeness magnitude of the theoretical GR distribution
+        delta_m : discretization of the magnitudes, important for the correct
+                visualization of the data
+        color   : color of the data. If theoretical GR distribution should be
+            colored differently this should be a list with two entries
+        size    : size of scattered data
+        grid    : bool, include grid lines or not
+        left    : When True, left edges of bins are returned. When false,
+                center points are returned.
+
+    Returns:
+        ax that was plotted on
+    """
+
+    mags = mags[~np.isnan(mags)]
+    mags = bin_to_precision(mags, delta_m)
+
+    bins, c_counts = get_cum_fmd(mags, delta_m, left=left)
+
+    if ax is None:
+        ax = plt.subplots()[1]
+
+    if b_value is not None:
+        if mc is None:
+            mc = min(mags)
+        x = mags[mags >= mc]
+        y = gutenberg_richter(x, b_value, mc, len(x))
+        if type(color) is not list:
+            color = [color, color]
+        ax.plot(x - delta_m / 2, y, color=color[1])
+        ax.scatter(bins, c_counts, s=size,
+                   color=color[0], marker='s')
+    else:
+        ax.scatter(bins, c_counts, s=size,
+                   color=color, marker='s')
+
+    ax.set_yscale('log')
+    ax.set_xlabel('Magnitude')
+    ax.set_ylabel('N')
+
+    if grid is True:
+        ax.grid(True)
+        ax.grid(which='minor', alpha=0.3)
+
+    return ax
+
+
 def plot_fmd(
         mags: np.ndarray,
         delta_m: float = 0,
