bf806505ee4ff54a2d60363437b01ae9571e6fa7
Nicolas Schmid <nicolas@breiten.ch>
2023-08-17 15:36:38 +0200

    feat: remove now replaced download functions and factor out edwards magnitude conversion

--- catalog_tools/__init__.py
+++ catalog_tools/__init__.py
@@ -2,29 +2,17 @@
 # flake8: noqa
 
 # analysis
-from catalog_tools.analysis.estimate_beta import\
-    estimate_b_elst,\
-    estimate_b_utsu,\
-    estimate_beta_tinti,\
-    estimate_b_tinti,\
-    estimate_b_laplace,\
-    shi_bolt_confidence
-
-# download
-from catalog_tools.download.download_catalogs import\
-    download_catalog_sed,\
-    download_catalog_scedc,\
-    download_catalog
-
+from catalog_tools.analysis.estimate_beta import (estimate_b_elst,
+                                                  estimate_b_laplace,
+                                                  estimate_b_tinti,
+                                                  estimate_b_utsu,
+                                                  estimate_beta_tinti,
+                                                  shi_bolt_confidence)
 # plots
-from catalog_tools.plots.basics import\
-    plot_cum_fmd,\
-    plot_fmd,\
-    plot_cum_count,\
-    plot_mags_in_time
+from catalog_tools.plots.basics import (plot_cum_count, plot_cum_fmd, plot_fmd,
+                                        plot_mags_in_time)
 from catalog_tools.plots.seismicity import plot_in_space
-
 # utils
 from catalog_tools.utils.binning import bin_to_precision
-from catalog_tools.utils.simulate_distributions import simulate_magnitudes
 from catalog_tools.utils.filtering import cat_intersect_polygon
+from catalog_tools.utils.simulate_distributions import simulate_magnitudes
---
+++ catalog_tools/analysis/tests/test_magnitudes.py
@@ -0,0 +1,18 @@
+from numpy.testing import assert_almost_equal, assert_equal
+
+from catalog_tools.analysis.magnitudes import apply_edwards
+
+
+def test_apply_edwards():
+    mag_types = ['MLh', 'MLhc', 'Ml', 'Mw']
+    mags = [2.5, 3.5, 4.5, 5.5]
+
+    result = [apply_edwards(typ, mag).tolist()
+              for (typ, mag) in zip(mag_types, mags)]
+
+    verify = [['Mw_converted', 2.506875], ['Mw_converted', 3.2734749999999995],
+              ['Mw_converted', 4.138274999999999], ['Mw', 5.5]]
+
+    for i, res in enumerate(result):
+        assert_almost_equal(res[1], verify[i][1])
+        assert_equal(res[0], verify[i][0])
