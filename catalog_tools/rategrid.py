6710002611ef79df383e78ba8592a44a17912648
Nicolas Schmid <nicolas@breiten.ch>
2023-09-14 16:22:07 +0200

    feat: add support for time-based MultiIndex in RateGrid

--- catalog_tools/rategrid.py
+++ catalog_tools/rategrid.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import numpy as np
 import pandas as pd
 
 from catalog_tools.utils import _check_required_cols, require_cols
@@ -66,6 +67,12 @@ class RateGrid(pd.DataFrame):
         self.endtime = endtime if isinstance(
             endtime, pd.Timestamp) else pd.to_datetime(endtime)
 
+        if len(self.index.names) > 1:
+            try:
+                self.reindex_cell_id()
+            except AttributeError:
+                pass
+
     @property
     def _constructor(self):
         return _rategrid_constructor_with_fallback
@@ -88,6 +95,66 @@ class RateGrid(pd.DataFrame):
             set(self._required_cols)), inplace=inplace)
         if not inplace:
             return df
+
+    def add_time_index(self, endtime=True):
+        """
+        Create MultiIndex using starttime, optionally endtime and a cell
+        number for each spatial block.
+
+        Args:
+            endtime : bool, optional
+                If True, create MultiIndex with starttime and endtime.
+                Otherwise, create MultiIndex with only starttime.
+
+        Returns:
+            RateGrid
+        """
+        if not getattr(self, 'starttime', None) or \
+                not getattr(self, 'endtime', None):
+            raise AttributeError(
+                'starttime and endtime must be set to use this method')
+
+        index = (self.starttime, self.endtime) if endtime else self.starttime
+        names = ['starttime', 'endtime'] if endtime else ['starttime']
+
+        # rename the index to cell_id, will be set in constructor
+        self.index.name = 'cell_id'
+
+        df = pd.concat({index: self}, names=names)
+
+        # manually set the metadata attributes
+        for arg in self._metadata:
+            setattr(df, arg, getattr(self, arg))
+
+        return df
+
+    @require_cols(require=_required_cols)
+    def reindex_cell_id(self):
+        """
+        If the RateGrid has a MultiIndex which includes `cell_id`
+        as a level, this method will update the RateGrid's index to use
+        unique cell_id values.
+        """
+
+        if 'cell_id' in self.index.names:
+            cell_bounds = self[['longitude_min', 'longitude_max',
+                                'latitude_min', 'latitude_max',
+                                'depth_min', 'depth_max']]
+
+            self['cell'] = np.unique(
+                cell_bounds, axis=0, return_inverse=True, equal_nan=True)[1]
+
+            self.set_index('cell', append=True, drop=True, inplace=True)
+            self.index = self.index.droplevel('cell_id')
+
+            self.index.set_names('cell_id', level='cell', inplace=True)
+
+        if 'starttime' in self.index.names:
+            self.starttime = self.index.get_level_values('starttime').min()
+            if 'endtime' in self.index.names:
+                self.endtime = self.index.get_level_values('endtime').max()
+            else:
+                self.endtime = self.index.get_level_values('starttime').max()
 
 
 class ForecastRateGrid(RateGrid):
