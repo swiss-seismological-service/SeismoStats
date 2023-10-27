64a4e868105bb9135e438893610d4cd30548343a
Aron Mirwald <aron.mirwald@sed.ethz.ch>
2023-10-27 12:11:20 +0200

    use google tiles instead of stamen tiles (which will be out of service by the end of the month)

--- catalog_tools/plots/seismicity.py
+++ catalog_tools/plots/seismicity.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from cartopy.io import shapereader
 from cartopy.mpl.gridliner import LATITUDE_FORMATTER, LONGITUDE_FORMATTER
+
 # for map plotting
 from shapely.geometry import Polygon
 
@@ -15,14 +16,15 @@ from catalog_tools.plots.basics import dot_size
 
 
 def plot_in_space(
-        cat: pd.DataFrame,
-        resolution: str = '110m',
-        include_map: bool | None = False,
-        country: str | None = None,
-        colors: str | None = None,
-        dot_smallest: int = 10,
-        dot_largest: int = 200,
-        dot_interpolation_power: int = 2,
+    cat: pd.DataFrame,
+    resolution: str = "10m",
+    include_map: bool | None = False,
+    country: str | None = None,
+    colors: str | None = None,
+    style: str = "satellite",
+    dot_smallest: int = 10,
+    dot_largest: int = 200,
+    dot_interpolation_power: int = 2,
 ) -> cartopy.mpl.geoaxes.GeoAxes:
     """
      Function plots seismicity on the surface. If include_map is chosen True,
@@ -31,16 +33,17 @@ def plot_in_space(
     according to the midpoint latitude.
 
     Args:
-        cat: dataframe- needs to have latitude, longitude and depth as
-            entries
-        resolution: resolution of map, '10m', '50m' and '110m' available
-        include_map: if True, seismicity will be plotted on natural earth
-            map, otherwise it will be plotted on a blank grid.
-        country: name of country, if None map will fit to data points
-        colors: color of background. if None is chosen, is will be either
-            white or standard natural earth colors.
-        dot_smallest: smallest dot size for magnitude scaling
-        dot_largest:largest dot size for magnitude scaling
+        cat:            dataframe- needs to have latitude, longitude and
+                        depth as entries
+        resolution:     resolution of map, '10m', '50m' and '110m' available
+        include_map:    if True, seismicity will be plotted on natural earth
+                        map, otherwise it will be plotted on a blank grid.
+        country:        name of country, if None map will fit to data points
+        colors:         color of background. if None is chosen, is will be
+                        either white or standard natural earth colors.
+        style:          style of map, 'satellite' or 'street' are available
+        dot_smallest:   smallest dot size for magnitude scaling
+        dot_largest:    largest dot size for magnitude scaling
         dot_interpolation_power: interpolation power for scaling
 
     Returns:
@@ -48,53 +51,66 @@ def plot_in_space(
     """
     # request data for use by geopandas
     if include_map is True:
-        category: str = 'cultural'
-        name: str = 'admin_0_countries'
+        category: str = "cultural"
+        name: str = "admin_0_countries"
         shpfilename = shapereader.natural_earth(resolution, category, name)
         df = geopandas.read_file(shpfilename)
 
     if colors is not None:
-        stamen_terrain = cimgt.Stamen('terrain-background',
-                                      desired_tile_form="L")
+        tiles = cimgt.GoogleTiles(style=style, desired_tile_form="L")
         plt.set_cmap(colors)
     else:
-        stamen_terrain = cimgt.Stamen('terrain-background')
+        tiles = cimgt.GoogleTiles(style=style)
 
     # projections that involved
-    st_proj = stamen_terrain.crs  # projection used by Stamen images
+    tile_proj = tiles.crs  # projection used by tiles
     ll_proj = ccrs.PlateCarree()  # CRS for raw long/lat
 
-    ax = plt.subplot(projection=st_proj)
+    ax = plt.subplot(projection=tile_proj)
 
     if include_map is True and country is not None:
         # create box around country
-        poly = [df.loc[df['ADMIN'] == country]['geometry'].values[0]]
+        poly = [df.loc[df["ADMIN"] == country]["geometry"].values[0]]
         pad_lat = abs(poly[0].bounds[0] - poly[0].bounds[2]) * 0.05
         pad_lon = abs(poly[0].bounds[1] - poly[0].bounds[3]) * 0.05
-        exts = [poly[0].bounds[0] - pad_lat, poly[0].bounds[2] + pad_lat,
-                poly[0].bounds[1] - pad_lon, poly[0].bounds[3] + pad_lon]
+        exts = [
+            poly[0].bounds[0] - pad_lat,
+            poly[0].bounds[2] + pad_lat,
+            poly[0].bounds[1] - pad_lon,
+            poly[0].bounds[3] + pad_lon,
+        ]
         msk = Polygon(rect_from_bound(*exts)).difference(
-            poly[0].simplify(0.01))
-        msk_stm = st_proj.project_geometry(msk, ll_proj)
-        ax.add_geometries(msk_stm, st_proj, facecolor='white',
-                          edgecolor='grey', alpha=0.6)
+            poly[0].simplify(0.01)
+        )
+        msk_stm = tile_proj.project_geometry(msk, ll_proj)
+        ax.add_geometries(
+            msk_stm, tile_proj, facecolor="white", edgecolor=None, alpha=0.6
+        )
     else:
         # create box around the data points
-        pad_lat = abs(max(cat['latitude']) - min(cat['latitude'])) * 0.05
-        pad_lon = abs(max(cat['longitude']) - min(cat['longitude'])) * 0.05
-        exts = [min(cat['longitude']) - pad_lon,
-                max(cat['longitude']) + pad_lon,
-                min(cat['latitude']) - pad_lat,
-                max(cat['latitude']) + pad_lat]
+        pad_lat = abs(max(cat["latitude"]) - min(cat["latitude"])) * 0.05
+        pad_lon = abs(max(cat["longitude"]) - min(cat["longitude"])) * 0.05
+        exts = [
+            min(cat["longitude"]) - pad_lon,
+            max(cat["longitude"]) + pad_lon,
+            min(cat["latitude"]) - pad_lat,
+            max(cat["latitude"]) + pad_lat,
+        ]
 
     ax.set_extent(exts, crs=ll_proj)
 
     if include_map is True:
-        ax.add_image(stamen_terrain, 8, alpha=0.6)
+        ax.add_image(tiles, 8, alpha=0.6)
 
     # gridlines
-    gl = ax.gridlines(crs=ll_proj, draw_labels=True,
-                      linewidth=1, color='gray', alpha=0.5, linestyle='--')
+    gl = ax.gridlines(
+        crs=ll_proj,
+        draw_labels=True,
+        linewidth=1,
+        color="gray",
+        alpha=0.5,
+        linestyle="--",
+    )
     gl.xformatter = LONGITUDE_FORMATTER
     gl.yformatter = LATITUDE_FORMATTER
     gl.bottom_labels = False
@@ -103,20 +119,26 @@ def plot_in_space(
     ax.scatter(
         cat["longitude"],
         cat["latitude"],
-        c='blue',
-        edgecolor='k',
-        s=dot_size(cat["magnitude"], smallest=dot_smallest, largest=dot_largest,
-                   interpolation_power=dot_interpolation_power),
+        c="blue",
+        edgecolor="k",
+        s=dot_size(
+            cat["magnitude"],
+            smallest=dot_smallest,
+            largest=dot_largest,
+            interpolation_power=dot_interpolation_power,
+        ),
         zorder=100,
         transform=ccrs.PlateCarree(),
-        linewidth=0.5, alpha=0.8,
+        linewidth=0.5,
+        alpha=0.8,
     )
 
     return ax
 
 
-def rect_from_bound(xmin: float, xmax: float, ymin: float, ymax: float
-                    ) -> list[tuple]:
+def rect_from_bound(
+    xmin: float, xmax: float, ymin: float, ymax: float
+) -> list[tuple]:
     """
     Makes list of tuples for creating a rectangle polygon
     Args:
