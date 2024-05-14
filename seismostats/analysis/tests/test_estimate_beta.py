66694b4b8051c7a615fee23ced73b020399e61b6
Nicolas Schmid <nicolas@breiten.ch>
2024-05-14 10:29:16 +0200

    style: some fixes

--- seismostats/analysis/tests/test_estimate_beta.py
+++ seismostats/analysis/tests/test_estimate_beta.py
@@ -211,16 +211,16 @@ def test_estimate_b_laplace(
     assert abs(b - b_estimate) / b <= precision
 
 
-def _create_test_catalogue_poisson(
+def _create_test_catalog_poisson(
         a_val_true: float,
         b_val_true: float):
     """
-    creates a synthetic catalogue with magnitudes
+    creates a synthetic catalog with magnitudes
     between 4 and 7.9 with unequal completeness periods. To be used
     for testing relevant recurrence parameter estimators.
     """
 
-    # assume a catalogue from year 1000 to end of 1999
+    # assume a catalog from year 1000 to end of 1999
     # with completeness as follows:
     completeness_table = np.array([[3.95, 1940],
                                    [4.95, 1880],
@@ -256,20 +256,16 @@ def _create_test_catalogue_poisson(
             np.array(['%d-06-15' % i for i in obs_yearsi], dtype='datetime64'))
 
     # add some earthquakes in incomplete years
-    mags_inc = np.concatenate(
-        [
-            np.random.randint(40, 50, 100) / 10,
-            np.random.randint(50, 60, 10) / 10,
-            np.random.randint(60, 70, 1) / 10,
-        ]
-    )
-    years_inc = np.concatenate(
-        [
-            np.random.randint(1000, 1940, 100),
-            np.random.randint(1000, 1880, 10),
-            np.random.randint(1000, 1500, 1),
-        ]
-    )
+    mags_inc = np.concatenate([
+        np.random.randint(40, 50, 100) / 10,
+        np.random.randint(50, 60, 10) / 10,
+        np.random.randint(60, 70, 1) / 10,
+    ])
+    years_inc = np.concatenate([
+        np.random.randint(1000, 1940, 100),
+        np.random.randint(1000, 1880, 10),
+        np.random.randint(1000, 1500, 1),
+    ])
     dates_inc = np.array(
         ["%d-06-15" % i for i in years_inc], dtype="datetime64"
     )
@@ -287,50 +283,39 @@ def _create_test_catalogue_poisson(
 def test_estimate_b_weichert(a_val_true: float,
                              b_val_true: float,
                              precision: float):
-    mags, dates = _create_test_catalogue_poisson(
+    mags, dates = _create_test_catalog_poisson(
         a_val_true, b_val_true)
 
-    (
-        b_val,
-        std_b_val,
-        rate_at_mref,
-        std_rate_at_mref,
-        a_val,
-    ) = estimate_b_weichert(
-        magnitudes=mags,
-        dates=dates,
-        completeness_table=np.array(
-            [[3.95, 1940], [4.95, 1880], [5.95, 1500], [6.95, 1000]]
-        ),
-        mag_max=7.95,
-        last_year=2000,
-        delta_m=0.1,
-        b_parameter="b_value",
-    )
+    (b_val, std_b_val, rate_at_mref, std_rate_at_mref, a_val,) = \
+        estimate_b_weichert(magnitudes=mags,
+                            dates=dates,
+                            completeness_table=np.array(
+                                [[3.95, 1940],
+                                 [4.95, 1880],
+                                 [5.95, 1500],
+                                 [6.95, 1000]]
+                            ),
+                            mag_max=7.95,
+                            last_year=2000,
+                            delta_m=0.1,
+                            b_parameter="b_value",
+                            )
 
     assert abs(b_val_true - b_val) / b_val_true <= precision
     assert abs(a_val_true - a_val) / a_val_true <= precision
 
 
 # load data for test_shi_bolt_confidence
-with open(
-    "seismostats/analysis/tests/data/test_shi_bolt_confidence.p", "rb"
-) as f:
+with open("seismostats/analysis/tests/data/test_shi_bolt_confidence.p",
+          "rb") as f:
     data = pickle.load(f)
 
 
-@pytest.mark.parametrize(
-    "magnitudes,b,b_parameter,std",
-    [
-        data["values_test1"],
-        data["values_test2"],
-    ],
-)
-def test_shi_bolt_confidence(
-    magnitudes: np.ndarray, b: float, b_parameter: str, std: float
-):
+@pytest.mark.parametrize("magnitudes,b,b_parameter,std",
+                         [data["values_test1"], data["values_test2"]],)
+def test_shi_bolt_confidence(magnitudes: np.ndarray,
+                             b: float, b_parameter: str,
+                             std: float):
     precision = 1e-10
-    assert (
-        shi_bolt_confidence(magnitudes, b=b, b_parameter=b_parameter) - std
-        < precision
-    )
+    assert (shi_bolt_confidence(magnitudes, b=b, b_parameter=b_parameter) - std
+            < precision)
