cc349acda755580924d42204f3fcde9bdb3b3404
Nicolas Schmid <nicolas@breiten.ch>
2024-05-21 17:09:19 +0200

    docs: add custom option for napoleon to support multiple return arguments

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ extensions = [
 
 templates_path = ['_templates']
 exclude_patterns = []
-
+napoleon_custom_sections = [('Returns', 'params_style')]
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
