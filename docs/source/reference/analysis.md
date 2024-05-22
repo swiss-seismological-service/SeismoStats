7eb62695be36a087c3f63e83f2f58244d87462bd
Leila Mizrahi <lemizrahi@gmail.com>
2024-05-22 14:12:01 +0200

    docs: add automatic b-value calculation documentation

---
+++ docs/source/reference/analysis.md
@@ -0,0 +1,20 @@
+# Analysis
+
+```{eval-rst}
+.. currentmodule:: seismostats
+```
+
+## Estimating b-Values
+
+```{eval-rst}
+.. autosummary::
+    :toctree: api/
+
+    estimate_b
+    analysis.estimate_b_tinti
+    analysis.estimate_b_positive
+    analysis.estimate_b_utsu
+    analysis.estimate_b_laplace
+    analysis.estimate_b_weichert
+    analysis.estimate_b_kijko_smit_2012
+```
---
+++ seismostats/analysis/__init__.py
@@ -0,0 +1,8 @@
+# flake8: noqa
+
+from seismostats.analysis.estimate_beta import (estimate_b_kijko_smit_2012,
+                                                estimate_b_laplace,
+                                                estimate_b_positive,
+                                                estimate_b_tinti,
+                                                estimate_b_utsu,
+                                                estimate_b_weichert)
