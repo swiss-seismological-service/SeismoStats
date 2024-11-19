c074009f61bd6b9d775fadc067ac55d93e80ecb7
Gottschali <aligottschall@protonmail.com>
2024-11-19 11:51:38 +0100

    chore!: remove Aftern
    Not needed according to Laurentiu

--- seismostats/analysis/declustering/__init__.py
+++ seismostats/analysis/declustering/__init__.py
@@ -1,6 +1,5 @@
 # flake8: noqa
 
-from seismostats.analysis.declustering.dec_afteran import Afteran
 from seismostats.analysis.declustering.dec_gardner_knopoff import GardnerKnopoffType1
 from seismostats.analysis.declustering.base import ShockTypes
 from seismostats.analysis.declustering.distance_time_windows import (
