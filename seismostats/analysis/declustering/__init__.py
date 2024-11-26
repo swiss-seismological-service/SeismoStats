a58f1574f02e3e7c49b3b5b008d33a192f77e172
Gottschali <aligottschall@protonmail.com>
2024-11-26 13:12:02 +0100

    refactor! declustering api
    - configure declusterer with __init__
      - take configured windows
      - kwargs instead of config dicts
    - _decluster for the actual declustering
    - base class gives __call__ as an entrypoint

--- seismostats/analysis/declustering/__init__.py
+++ seismostats/analysis/declustering/__init__.py
@@ -1,7 +1,6 @@
 # flake8: noqa
 
 from seismostats.analysis.declustering.dec_gardner_knopoff import GardnerKnopoffType1
-from seismostats.analysis.declustering.base import ShockTypes
 from seismostats.analysis.declustering.distance_time_windows import (
     UhrhammerWindow, GardnerKnopoffWindow, GruenthalWindow
 )
