0734ecb8ad7adc8ead276ef67a667ed69269abe3
Leila Mizrahi <lemizrahi@gmail.com>
2024-12-06 11:39:32 +0100

    feature: uncertainty with weights

--- seismostats/analysis/bvalue/base.py
+++ seismostats/analysis/bvalue/base.py
@@ -70,7 +70,15 @@ class BValueEstimator(ABC):
         Shi and Bolt estimate of the beta/b-value estimate.
         '''
         assert self.__b_value is not None, 'Please run the estimator first.'
+        if get_option('warnings') is True:
+            if self.weights is not None:
+                warnings.warn(
+                    'Shi and Bolt confidence with weights considers the magnitudes as '
+                    'having length {}, the sum of relevant weights.'.format(
+                        np.sum(self.weights))
+                )
         return shi_bolt_confidence(self.magnitudes,
+                                   weights=self.weights,
                                    b=self.__b_value,
                                    b_parameter=self.__b_parameter)
 
@@ -97,6 +105,7 @@ class BValueEstimator(ABC):
             assert len(self.magnitudes) == len(self.weights), (
                 "The number of magnitudes and weights must be equal."
             )
+            assert np.all(self.weights >= 0), "Weights must be nonnegative."
 
         # test if lowest magnitude is much larger than mc
         if get_option("warnings") is True:
