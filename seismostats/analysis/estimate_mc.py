e9d4005185590da7c2c6b3965641bf997dd0fd6b
Leila Mizrahi <lemizrahi@gmail.com>
2024-12-13 15:43:03 +0100

    fix: define x_bins correctly when ks_ds are given

--- seismostats/analysis/estimate_mc.py
+++ seismostats/analysis/estimate_mc.py
@@ -115,6 +115,7 @@ def ks_test_gr(
             np.arange(mc, max_considered_mag + 3
                       / 2 * delta_m, delta_m), delta_m
         )
+        x_bins -= delta_m / 2
         x = bin_to_precision((x_bins[1:] + x_bins[:-1]) / 2, delta_m)
         _, y_th = cdf_discrete_GR(x, mc=mc, delta_m=delta_m, beta=beta)
 
