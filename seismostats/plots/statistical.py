f4b5019dffc9e025c188d2ff475c88df17635209
Aron Mirwald <aron.mirwald@sed.ethz.ch>
2024-12-19 12:45:17 +0100

    adjusted plotting function such that it is easier to understand what it does

--- seismostats/plots/statistical.py
+++ seismostats/plots/statistical.py
@@ -11,8 +11,8 @@ from seismostats.analysis.bvalue.classic import ClassicBValueEstimator
 
 def plot_mc_vs_b(
     magnitudes: np.ndarray,
-    mcs: float | np.ndarray,
-    dmcs: float | np.ndarray | None = None,
+    mcs: np.ndarray,
+    dmc: float | None = None,
     delta_m: float = 0.1,
     b_method: BValueEstimator = ClassicBValueEstimator,
     confidence_intvl: float = 0.95,
@@ -24,13 +24,11 @@ def plot_mc_vs_b(
     Args:
         magnitudes: magnitudes of the catalog
         mcs:        completeness magnitudes (list or numpy array)
+        dmc:        if a positive b-value estimator is used, this is the
+                minimum difference that is considered. For the
+                ClassicBValueEstimator, leave this as the default None.
         delta_m:    discretization of the magnitudes
-        method:     method used for b-value estimation, either 'classic' or
-                    'positive' or 'positive_postcut'. positive_postcut is the
-                    same as 'positive' but with the postcut method (differences
-                    are taken before cutting the magnitudes below the
-                    completeness magnitude). The mcs are then interpreted as
-                    dmcs.
+        method:     method used for b-value estimation
         confidence_intvl:   confidence interval that should be plotted
         ax:         axis where figure should be plotted
         color:      color of the data
@@ -42,26 +40,13 @@ def plot_mc_vs_b(
     b_values = []
     b_errors = []
 
-    # if dmc is None, the function just iterates through the mcs
-    if dmcs is None:
-        variable = mcs
-        var_string = 'Completeness magnitude $m_c$'
+    if dmc is None:
         for mc in mcs:
             estimator = b_method(mc=mc, delta_m=delta_m)
             b_values.append(estimator(magnitudes))
             b_errors.append(estimator.std)
-    # if dmc is given, the function iterates though the dmc and mc values
     else:
-        if isinstance(dmcs, (float, int)):
-            variable = mcs
-            var_string = 'Completeness magnitude $m_c$'
-            dmcs = [dmcs] * len(mcs)
-        elif isinstance(mcs, (float, int)):
-            variable = dmcs
-            var_string = 'Minimum magnitude difference $dm_c$'
-            mcs = [mcs] * len(dmcs)
-
-        for mc, dmc in zip(mcs, dmcs):
+        for mc in mcs:
             estimator = b_method(mc=mc, delta_m=delta_m, dmc=dmc)
             b_values.append(estimator(magnitudes))
             b_errors.append(estimator.std)
@@ -74,15 +59,15 @@ def plot_mc_vs_b(
 
     # Plotting: this either
     error_factor = norm.ppf((1 + confidence_intvl) / 2)
-    ax.plot(variable, b_values, "-o", color=color)
+    ax.plot(mcs, b_values, "-o", color=color)
     ax.fill_between(
-        variable,
+        mcs,
         b_values - error_factor * b_errors,
         b_values + error_factor * b_errors,
         alpha=0.2,
         color=color,
     )
-    ax.set_xlabel(var_string)
+    ax.set_xlabel("Completeness magnitude $m_c$")
     ax.set_ylabel("b-value")
     ax.grid(True)
 
