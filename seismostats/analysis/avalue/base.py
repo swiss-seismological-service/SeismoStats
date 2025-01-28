7fd9f8adedb35be4a2b3c6f458f72927ad3399d6
Gottschali <aligottschall@protonmail.com>
2025-01-28 16:58:54 +0100

    chore: _filtering and arons changes

--- seismostats/analysis/avalue/base.py
+++ seismostats/analysis/avalue/base.py
@@ -17,47 +17,58 @@ class AValueEstimator(ABC):
         self.b_value: float | None = None
         self.delta_m: float | None = None
         self.weights: np.ndarray | None = None
+        self.times: np.ndarray | None = None
 
         self.__a_value: float | None = None
 
     def calculate(self,
                   magnitudes: np.ndarray,
-                  delta_m: float,
                   mc: float,
+                  delta_m: float,
                   scaling_factor: float | None = None,
                   m_ref: float | None = None,
                   b_value: float | None = None,
                   *args,
                   weights: np.ndarray | list | None = None,
+                  times: np.ndarray | list | None = None,
                   **kwargs) -> float:
-        '''
+        r'''
         Return the a-value estimate.
+
+        .. math::
+        N(m) = 10 ^ {a - b \cdot (m - m_{ref})}
 
         Args:
             magnitudes: Array of magnitudes
-            delta_m:    Discretization of magnitudes.
             mc:         Completeness magnitude
+            delta_m:    Discretization of magnitudes.
             scaling_factor:     scaling factor.
                 If given, this is used to normalize
                 the number of observed events. For example:
                 Volume or area of the region considered
                 or length of the time interval, given in the unit of interest.
-            m_ref: reference magnitude for which the a-value is estimated. If
-                None, m_ref is set to the smallest magnitude in the catalog.
-            b_value: b-value of the Gutenberg-Richter law. Only relevant
+            m_ref:      reference magnitude for which the a-value is estimated.
+            b_value:    b-value of the Gutenberg-Richter law. Only relevant
                 when m_ref is not None.
             weights:    Array of weights for the magnitudes.
+            times:      vector of times of the events, in any format (datetime,
+                float, etc.)
+
+        Returns:
+            a: a-value of the Gutenberg-Richter law
         '''
 
         self.magnitudes = np.array(magnitudes)
+        self.mc = mc
+        self.delta_m = delta_m
         self.scaling_factor = scaling_factor
         self.m_ref = m_ref
-        self.mc = mc
         self.b_value = b_value
-        self.delta_m = delta_m
         self.weights = None if weights is None else np.array(weights)
+        self.times = None if times is None else np.array(times)
 
         self._sanity_checks()
+        self._filtering()
 
         self.__a_value = self._estimate(*args, **kwargs)
         return self.__a_value
@@ -69,6 +80,21 @@ class AValueEstimator(ABC):
         '''
         pass
 
+    def _filtering(self) -> np.ndarray:
+        '''
+        Filter out magnitudes below the completeness magnitude.
+        '''
+        idx = self.magnitudes >= self.mc - self.delta_m / 2
+        self.magnitudes = self.magnitudes[idx]
+
+        if self.weights is not None:
+            self.weights = self.weights[idx]
+
+        if self.times is not None:
+            self.times = self.times[idx]
+
+        return idx
+
     def _sanity_checks(self):
         '''
         Perform sanity checks on the input data.
@@ -79,7 +105,8 @@ class AValueEstimator(ABC):
         # else:
         #     tolerance = max(self.delta_m / 100, 1e-08)
         # assert (
-        #     binning_test(self.magnitudes, self.delta_m, tolerance)
+        #     binning_test(self.magnitudes, self.delta_m, tolerance,
+        #                  check_larger_binning=False))
         # )
         # 'Magnitudes are not binned correctly.'
 
@@ -89,8 +116,18 @@ class AValueEstimator(ABC):
             )
             assert np.all(self.weights >= 0), 'Weights must be nonnegative.'
 
+        if self.times is not None:
+            assert len(self.magnitudes) == len(self.times), (
+                'The number of magnitudes and times must be equal.'
+            )
+
         # test if lowest magnitude is much larger than mc
         if get_option('warnings') is True:
+            if np.min(self.magnitudes) < self.mc:
+                warnings.warn(
+                    "Completeness magnitude is higher "
+                    "than the lowest magnitude."
+                    "Cutting the magnitudes to the completeness magnitude.")
             if np.min(self.magnitudes) - self.mc > self.delta_m / 2:
                 warnings.warn(
                     'No magnitudes in the lowest magnitude bin are present. '
@@ -99,8 +136,6 @@ class AValueEstimator(ABC):
 
     def _reference_scaling(self, a: float) -> float:
         '''
-        Scale the a-value to a reference magnitude and b-value.
-
         Args:
             a: a-value
 
--- seismostats/analysis/avalue/positive.py
+++ seismostats/analysis/avalue/positive.py
@@ -26,50 +26,49 @@ class APositiveAValueEstimator(AValueEstimator):
     def calculate(self,
                   magnitudes: np.ndarray,
                   times: np.ndarray,
+                  mc: float,
+                  delta_m: float,
                   dmc: float | None = None,
-                  correction: bool = False,
+                  scaling_factor: float | None = None,
+                  m_ref: float | None = None,
+                  b_value: float | None = None,
                   *args,
+                  weights: np.ndarray | list | None = None,
                   **kwargs) -> float:
         """
         Args:
             magnitudes: Array of magnitudes
             times: vector of times of the events, in any format (datetime,
                 float, etc.)
+            mc:         Completeness magnitude
+            delta_m:    Discretization of magnitudes.
             dmc:        minimum magnitude difference between consecutive events.
                 If None, the default value is delta_m.
-            correction: If True,
-                the a-value is corrected for the bias introduced
-                by the observation period being larger than the time interval
-                between the first and last event. This is only relevant if the
-                sample is very small (<30). The assumption is that the blind
-                time in the beginning and end of the catalogue has a similar
-                distribution as the rest of the catalogue. We think that this
-                improves the estimate of the a-value for small samples, however,
-                without proof.
+            magnitudes: Array of magnitudes
+            scaling_factor:     scaling factor.
+                If given, this is used to normalize
+                the number of observed events. For example:
+                Volume or area of the region considered
+                or length of the time interval, given in the unit of interest.
+            m_ref:      reference magnitude for which the a-value is estimated.
+            b_value:    b-value of the Gutenberg-Richter law. Only relevant
+                when m_ref is not None.
+            weights:    Array of weights for the magnitudes.
         """
         return super().calculate(magnitudes,
                                  times=times,
+                                 mc=mc,
+                                 delta_m=delta_m,
                                  dmc=dmc,
-                                 correction=correction,
+                                 scaling_factor=scaling_factor,
+                                 m_ref=m_ref,
+                                 b_value=b_value,
+                                 weights=weights,
                                  *args,
                                  **kwargs)
 
     def _estimate(self,
-                  times: np.ndarray,
-                  dmc: float | None = None,
-                  correction: bool = False) -> float:
-
-        if self.mc is None:
-            self.mc = self.magnitudes.min()
-        elif self.magnitudes.min() < self.mc:
-            if get_option("warnings") is True:
-                warnings.warn(
-                    "Completeness magnitude is higher"
-                    "than the lowest magnitude."
-                    "Cutting the magnitudes to the completeness magnitude.")
-            idx = self.magnitudes >= self.mc - self.delta_m / 2
-            self.magnitudes = self.magnitudes[idx]
-            times = times[idx]
+                  dmc: float | None = None) -> float:
         if dmc is None:
             dmc = self.delta_m
         elif dmc < 0:
@@ -78,9 +77,9 @@ class APositiveAValueEstimator(AValueEstimator):
             warnings.warn("dmc is smaller than delta_m, not recommended")
 
         # order the magnitudes and times
-        idx = np.argsort(times)
+        idx = np.argsort(self.times)
         self.magnitudes = self.magnitudes[idx]
-        times = times[idx]
+        times = self.times[idx]
 
         # differences
         mag_diffs = np.diff(self.magnitudes)
@@ -93,11 +92,9 @@ class APositiveAValueEstimator(AValueEstimator):
 
         # estimate the number of events within the time interval
         total_time = times[-1] - times[0]
-        total_time_pos = sum(time_diffs / total_time)
-        if correction:
-            total_time += 2 * np.mean(np.diff(times) / total_time)
-            total_time_pos += np.mean(time_diffs / total_time)
-        n_pos = 1 / total_time_pos * len(mag_diffs)
+
+        time_factor = sum(time_diffs / total_time)
+        n_pos = sum(idx) / time_factor
 
         # estimate a-value
         a = np.log10(n_pos)
