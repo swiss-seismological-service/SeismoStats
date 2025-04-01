a3433e6fab90a70abd442ce91730b8bb9ac95263
Aron Mirwald <aron.mirwald@sed.ethz.ch>
2025-04-01 10:41:41 +0900

    renamed sample to magnitudes in mc functions

--- seismostats/analysis/estimate_mc.py
+++ seismostats/analysis/estimate_mc.py
@@ -42,7 +42,7 @@ def cdf_discrete_exp(
 
 
 def ks_test_gr(
-    sample: np.ndarray,
+    magnitudes: np.ndarray,
     mc: float,
     delta_m: float,
     b_value: float,
@@ -57,7 +57,7 @@ def ks_test_gr(
     parameters can be rejected.
 
     Args:
-        sample:     Array of magnitudes.
+        magnitudes: Array of magnitudes.
         mc:         Completeness magnitude.
         delta_m:    Bin size of discretized magnitudes.
         b_value:    b-value of the Gutenberg-Richter law.
@@ -74,14 +74,14 @@ def ks_test_gr(
         ks_ds:      Array of KS distances.
     """
     if get_option("warnings") is True:
-        if np.min(sample) < mc - delta_m / 2:
+        if np.min(magnitudes) < mc - delta_m / 2:
             warnings.warn("Sample contains values below mc.")
 
-        if len(sample) == 0:
+        if len(magnitudes) == 0:
             warnings.warn("No sample given.")
             return 0, 1, []
 
-        if len(np.unique(sample)) == 1:
+        if len(np.unique(magnitudes)) == 1:
             warnings.warn("Sample contains only one value.")
             return 0, 1, []
 
@@ -90,11 +90,11 @@ def ks_test_gr(
     if ks_ds is None:
         ks_ds = []
 
-        n_sample = len(sample)
+        n_sample = len(magnitudes)
         simulated_all = simulate_magnitudes_binned(
             n * n_sample, b_value, mc, delta_m, b_parameter="b_value"
         )
-        max_considered_mag = np.max([np.max(sample), np.max(simulated_all)])
+        max_considered_mag = np.max([np.max(magnitudes), np.max(simulated_all)])
 
         x_bins = bin_to_precision(
             np.arange(mc, max_considered_mag + 3
@@ -114,7 +114,7 @@ def ks_test_gr(
             ks_ds.append(ks_d)
 
     else:
-        max_considered_mag = np.max(sample)
+        max_considered_mag = np.max(magnitudes)
         x_bins = bin_to_precision(
             np.arange(mc, max_considered_mag + 3
                       / 2 * delta_m, delta_m), delta_m
@@ -123,7 +123,7 @@ def ks_test_gr(
         x_bins -= delta_m / 2
         _, y_th = cdf_discrete_exp(x, mc=mc, delta_m=delta_m, beta=beta)
 
-    y_hist, _ = np.histogram(sample, bins=x_bins)
+    y_hist, _ = np.histogram(magnitudes, bins=x_bins)
     y_emp = np.cumsum(y_hist) / np.sum(y_hist)
 
     ks_d_obs = np.max(np.abs(y_emp - y_th))
@@ -133,7 +133,7 @@ def ks_test_gr(
 
 
 def estimate_mc_ks(
-    sample: np.ndarray,
+    magnitudes: np.ndarray,
     delta_m: float,
     mcs_test: np.ndarray | None = None,
     p_pass: float = 0.1,
@@ -159,16 +159,16 @@ def estimate_mc_ks(
           Society of America, 92(4), pp.2333-2342.
 
     Args:
-        sample:             Array of magnitudes to test.
+        magnitudes:         Array of magnitudes to test.
         delta_m:            Bin size of discretized magnitudes. Sample has to be
                         rounded to bins beforehand).
         mcs_test:           Array of tested completeness magnitudes. If `None`,
-                        it will be generated automatically based on `sample`
-                        and `delta_m`.
+                        it will be generated automatically based on
+                        ``magnitudes`` and ``delta_m``.
         p_pass:             p-value required to pass the test.
         stop_when_passed:   Stop calculations when first mc passes the test.
-        b_value:            If `b_value` is 'known', only estimate `mc` assuming
-                        the given `b_value`.
+        b_value:            If ``b_value`` is 'known', only estimate ``mc``
+                        assuming the given ``b_value``.
         b_method:           b-value estimator to use if b-value needs to be
                         calculated from data
         n:                  Number of number of times the KS distance is
@@ -181,8 +181,8 @@ def estimate_mc_ks(
                         estimator.
 
     Returns:
-        best_mc:        `mc` for which the p-value is lowest.
-        best_b_value:   `b_value` corresponding to the best `mc`.
+        best_mc:        ``mc`` for which the p-value is lowest.
+        best_b_value:   ``b_value`` corresponding to the best ``mc``.
         mcs_test:       Tested completeness magnitudes.
         b_values_test:  Tested b-values.
         ks_ds:          KS distances.
@@ -191,19 +191,19 @@ def estimate_mc_ks(
 
     if mcs_test is None:
         mcs_test = bin_to_precision(
-            np.arange(np.min(sample), np.max(sample), delta_m), delta_m
+            np.arange(np.min(magnitudes), np.max(magnitudes), delta_m), delta_m
         )
     else:
         # sort mcs
         mcs_test = np.sort(np.unique(mcs_test))
 
     # if magnitude binning is incorrect, b-value calculation will fail anyway
-    if b_value is None and not binning_test(sample, delta_m,
+    if b_value is None and not binning_test(magnitudes, delta_m,
                                             check_larger_binning=False):
         raise ValueError('Magnitudes are not binned correctly.')
 
     if get_option("warnings") is True:
-        if not binning_test(sample, delta_m, check_larger_binning=False):
+        if not binning_test(magnitudes, delta_m, check_larger_binning=False):
             warnings.warn("Magnitudes are not binned correctly. "
                           "Test might fail because of this.")
         if not binning_test(mcs_test, delta_m, check_larger_binning=False):
@@ -225,7 +225,7 @@ def estimate_mc_ks(
         if verbose:
             print(f'\nTesting mc: {mc}.')
 
-        mc_sample = sample[sample >= mc - delta_m / 2]
+        mc_sample = magnitudes[magnitudes >= mc - delta_m / 2]
 
         # if no b_value is given, estimate b_value
         if b_value is None:
@@ -281,7 +281,7 @@ def estimate_mc_ks(
 
 
 def estimate_mc_maxc(
-    sample: np.ndarray,
+    magnitudes: np.ndarray,
     delta_m: float,
     correction_factor: float = 0.2,
 ) -> float:
@@ -300,7 +300,7 @@ def estimate_mc_maxc(
           Bulletin of the Seismological Society of America, 95(2), pp.684-698.
 
     Args:
-        sample:             Array of magnitudes to test.
+        magnitudes:             Array of magnitudes to test.
         delta_m:            Bin size of discretized magnitudes. Sample has to be
                         rounded to bins beforehand.
         correction_factor:  Correction factor for the maximum curvature
@@ -310,19 +310,19 @@ def estimate_mc_maxc(
         mc:                 Estimated completeness magnitude.
     """
     if get_option("warnings") is True:
-        if not binning_test(sample, delta_m, check_larger_binning=False):
+        if not binning_test(magnitudes, delta_m, check_larger_binning=False):
             warnings.warn("Magnitudes are not binned correctly. "
                           "Test might fail because of this.")
 
     bins, count, _ = get_fmd(
-        magnitudes=sample, delta_m=delta_m, bin_position="center"
+        magnitudes=magnitudes, delta_m=delta_m, bin_position="center"
     )
     mc = bins[count.argmax()] + correction_factor
     return mc
 
 
 def estimate_mc_b_stability(
-        sample: np.ndarray,
+        magnitudes: np.ndarray,
         delta_m: float,
         mcs_test: np.ndarray | None = None,
         stop_when_passed: bool = True,
@@ -346,12 +346,12 @@ def estimate_mc_b_stability(
         (2005): 684-698.
 
     Args:
-        sample:             Array of magnitudes.
+        magnitudes:         Array of magnitudes.
         delta_m:            Bin size of discretized magnitudes. Sample has to be
                         rounded to bins beforehand.
         mcs_test:           Array of tested completeness magnitudes. If None,
-                        it will be generated automatically based on the sample
-                        and delta_m.
+                        it will be generated automatically based on
+                        ``magnitudes`` and ``delta_m``.
         stop_when_passed:   Boolean that indicates whether to stop computation
                         when a completeness magnitude (mc) has passed the test.
         b_method:           b-value estimator to use for b-value calculation.
@@ -365,7 +365,7 @@ def estimate_mc_b_stability(
 
     Returns:
         best_mc:        Best magnitude of completeness estimate.
-        best_b_value:   b-value associated with best_mc.
+        best_b_value:   b-value associated with ``best_mc``.
         mcs_test:       Array of tested completeness magnitudes.
         b_values_test:  Array of b-values associated to tested mcs.
         diff_bs:        Array of differences divided by std, associated
@@ -375,7 +375,7 @@ def estimate_mc_b_stability(
     steps = len(np.arange(0, stability_range, delta_m))
 
     if mcs_test is None:
-        mcs_test = np.arange(np.min(sample), np.max(sample), delta_m)
+        mcs_test = np.arange(np.min(magnitudes), np.max(magnitudes), delta_m)
         mcs_test = bin_to_precision(mcs_test, delta_m)
         if len(mcs_test) <= steps:
             raise ValueError(
@@ -383,13 +383,13 @@ def estimate_mc_b_stability(
         mcs_test = mcs_test[:-steps + 1]
     else:
         mcs_test = np.array(mcs_test)
-        mcs_test = mcs_test[mcs_test + stability_range <= np.max(sample)]
+        mcs_test = mcs_test[mcs_test + stability_range <= np.max(magnitudes)]
         if len(mcs_test) < 1:
             raise ValueError(
                 "The range of magnitudes is smaller than the stability range.")
 
     # if magnitude binning is incorrect, b-value calculation will fail anyway
-    if not binning_test(sample, delta_m, check_larger_binning=False):
+    if not binning_test(magnitudes, delta_m, check_larger_binning=False):
         raise ValueError('Magnitudes are not binned correctly.')
 
     if get_option("warnings") is True:
@@ -409,7 +409,7 @@ def estimate_mc_b_stability(
         if verbose:
             print(f'\nTesting mc: {mc}.')
 
-        sample_selected = sample[sample >= mc - delta_m / 2]
+        sample_selected = magnitudes[magnitudes >= mc - delta_m / 2]
 
         if len(sample_selected) < 30:
             warnings.warn("Number of events above tested Mc is less than 30. "
@@ -421,10 +421,10 @@ def estimate_mc_b_stability(
         b_values_test.append(b)
 
         mc_plus = np.arange(mc, mc + stability_range, delta_m)
-        mc_plus = mc_plus[mc_plus <= np.max(sample)]
+        mc_plus = mc_plus[mc_plus <= np.max(magnitudes)]
         b_ex = []
         for mc_p in mc_plus:
-            sample_selected = sample[sample >= mc_p - delta_m / 2]
+            sample_selected = magnitudes[magnitudes >= mc_p - delta_m / 2]
             estimator.calculate(sample_selected, mc_p, delta_m, **kwargs)
             b_ex.append(estimator.b_value)
         b_avg = np.sum(b_ex) / steps
