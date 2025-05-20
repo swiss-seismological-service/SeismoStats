89d8af9bb4107674f9624d79e72a52b0d6b1b6f9
Aron Mirwald <aron.mirwald@sed.ethz.ch>
2025-05-20 16:44:36 +0200

    add p_lilliefors to the b-value estimators

--- seismostats/analysis/bvalue/utils.py
+++ seismostats/analysis/bvalue/utils.py
@@ -2,6 +2,7 @@ import warnings
 from typing import Literal
 
 import numpy as np
+import statsmodels.api as sm
 
 from seismostats.utils._config import get_option
 from seismostats.utils.binning import bin_to_precision
@@ -284,3 +285,35 @@ def ks_test_gr(
     p_val = sum(ks_ds >= ks_d_obs) / len(ks_ds)
 
     return p_val, ks_d_obs, ks_ds
+
+
+def ks_test_gr_lilliefors(
+    magnitudes: np.ndarray,
+    mc: float,
+) -> float:
+    """
+    Performs the Kolmogorov-Smirnov (KS) test for the Gutenberg-Richter
+    distribution for a given magnitude sample and mc and b-value, based on
+    the Lilliefors test. The magnitudes are assumed to be continuous. In order
+    to use the test for discrete magnitudes, the test should be performed
+    multiple times with different dithered magnitudes.
+
+    Source:
+        - Lilliefors, Hubert W. "On the Kolmogorov-Smirnov test for the
+        exponential distribution with mean unknown." Journal of the American
+        Statistical Association 64.325 (1969): 387-389.
+
+    Args:
+        magnitudes: Array of magnitudes.
+        mc:         Completeness magnitude.
+        delta_m:    Bin size of discretized magnitudes.
+
+    Returns:
+        p_val:      p-value.
+    """
+    mags_shifted = magnitudes - mc
+    out = sm.stats.diagnostic.lilliefors(
+        mags_shifted, dist='exp', pvalmethod='table')
+    p_val = out[1]
+
+    return p_val
