2ad1164f03cd2180382b6c566f9deae9cf948d8d
Aron Mirwald <aron.mirwald@sed.ethz.ch>
2025-11-05 15:59:53 +0100

    undo init change

--- seismostats/__init__.py
+++ seismostats/__init__.py
@@ -1,7 +1,6 @@
 from importlib.metadata import version
 
 __version__ = version("seismostats")
-from seismostats.utils._config import get_option, set_option  # noqa
 from seismostats.catalogs.catalog import Catalog, ForecastCatalog  # noqa
 from seismostats.catalogs.client import FDSNWSEventClient  # noqa
 from seismostats.catalogs.rategrid import ForecastGRRateGrid  # noqa
